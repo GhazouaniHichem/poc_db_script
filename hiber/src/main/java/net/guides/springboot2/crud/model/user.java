6bb954ef4a24ec33418a895ec854a220d74714ad
GhazouaniHichem <ghazouanih68@gmail.com>
2024-08-11 22:16:50 +0200

    first commit

---
+++ hiber/src/main/java/net/guides/springboot2/crud/model/user.java
@@ -0,0 +1,68 @@
+package net.guides.springboot2.crud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pc_address_view")
+public class Employee {
+
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "targetdate", nullable = false)
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	@Column(name = "effectivedate", nullable = false)
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Column(name = "validuntil", nullable = false)
+	public String getValiduntil() {
+		return validuntil;
+	}
+	public void setValiduntil(String emailId) {
+		this.validuntil = validuntil;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ "]";
+	}
+	
+}
